b29157f6907b8f3f77b834c9952960bbcc3bd37b
Andra Lally <alally@slack-corp.com>
2019-12-04 11:55:42 -0700

    add video length logic

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,7 +1,22 @@
 const electron = require("electron"); // running in node runtime
-const { app, BrowserWindow } = electron;
+const ffmpeg = require("fluent-ffmpeg"); // running in node runtime
+const { app, BrowserWindow, ipcMain } = electron;
 
+let mainWindow;
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+// ipcMain.on is what listens to events from the browser window
+ipcMain.on("video:submit", (e, path) => {
+  console.log(path);
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    //  mainWindow.webContents.send is send events to the browser
+    mainWindow.webContents.send("video:length", metadata.format.duration);
+  });
 });
